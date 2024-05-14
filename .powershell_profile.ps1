3326a3471a209247829fc7683daf972163d66f05
Muhammad Hafiz bin Muhamad <mhafiz.muhamad@gmail.com>
2024-05-14 15:21:36 +0800

    Added powershell profile.

---
+++ .powershell_profile.ps1
@@ -0,0 +1,116 @@
+# Reference:
+# 1. https://github.com/ChrisTitusTech/powershell-profile/blob/main/Microsoft.PowerShell_profile.ps1
+# 2. https://github.com/johnthebrit/RandomStuff/blob/master/PowerShellStuff/psreadlinedemo.ps1
+
+# New-PSDrive -PSProvider registry -Root HKEY_CLASSES_ROOT -Name HKCR
+# New-PSDrive -PSProvider registry -Root HKEY_USERS -Name HKU
+# New-PSDrive -PSProvider registry -Root HKEY_CURRENT_CONFIG -Name HKCC
+
+# Initial GitHub.com connectivity check with 1 second timeout
+$canConnectToGitHub = Test-Connection github.com -Count 1 -Quiet -TimeoutSeconds 1
+
+# Import Modules and External Profiles
+# Ensure Terminal-Icons module is installed before importing
+foreach ($module in @("PSReadLine", "Terminal-Icons", "7Zip4Powershell", "posh-git")){
+    if (-not (Get-Module -ListAvailable -Name $module)) {
+        Install-Module -Name $module -Scope CurrentUser -Force -SkipPublisherCheck
+    }
+}
+
+Import-Module PSReadLine
+Import-Module Terminal-Icons
+Import-Module 7Zip4Powershell
+Import-Module posh-git
+
+# Set UNIX-like aliases for the admin command, so sudo <command> will run the command with elevated rights.
+Set-Alias -Name su -Value admin
+Set-Alias -Name sudo -Value admin
+
+$versionMinimum = [Version]'7.1.999'
+if (($host.name -eq 'ConsoleHost') -and ($PSVersionTable.PSVersion -ge $versionMinimum)) {
+    Set-PSReadLineOption -PredictionSource HistoryAndPlugin
+}
+else {
+    Set-PSReadLineOption -PredictionSource History
+}
+Set-PSReadLineOption -PredictionViewStyle ListView
+Set-PSReadlineKeyHandler -Key Ctrl+Tab -Function TabCompleteNext
+Set-PSReadlineKeyHandler -Key Ctrl+Shift+Tab -Function TabCompletePrevious
+Set-PSReadlineKeyHandler -Key UpArrow -Function HistorySearchBackward
+Set-PSReadlineKeyHandler -Key DownArrow -Function HistorySearchForward
+
+$env:path += ";$env:localappdata\Notepad++"
+
+# Admin Check and Prompt Customization
+$isAdmin = ([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole]::Administrator)
+function prompt {
+    if ($isAdmin) { "[" + (Get-Location) + "] # " } else { "[" + (Get-Location) + "] $ " }
+}
+$adminSuffix = if ($isAdmin) { " [ADMIN]" } else { "" }
+$Host.UI.RawUI.WindowTitle = "PowerShell {0}$adminSuffix" -f $PSVersionTable.PSVersion.ToString()
+
+# Simple function to start a new elevated process. If arguments are supplied then
+# a single command is started with admin rights; if not then a new admin instance
+# of PowerShell is started.
+function admin {
+    if ($args.Count -gt 0) {
+        $argList = "& '" + $args + "'"
+        Start-Process "$psHome\pwsh.exe" -Verb runAs -ArgumentList $argList
+    }
+    else {
+        Start-Process "$psHome\pwsh.exe" -Verb runAs
+    }
+}
+
+# Set UNIX-like aliases for the admin command, so sudo <command> will run the command
+# with elevated rights.
+Set-Alias -Name su -Value admin
+Set-Alias -Name sudo -Value admin
+
+# Useful shortcuts for traversing directories
+function cd... { Set-Location ..\.. }
+function cd.... { Set-Location ..\..\.. }
+
+# Compute file hashes - useful for checking successful downloads
+function md5 { Get-FileHash -Algorithm MD5 $args }
+function sha1 { Get-FileHash -Algorithm SHA1 $args }
+function sha256 { Get-FileHash -Algorithm SHA256 $args }
+
+# Quick shortcut to start notepad
+function n { notepad $args }
+
+function np { notepad++ $args }
+
+function Get-PubIP { (Invoke-WebRequest http://ifconfig.me/ip ).Content }
+
+function uptime {
+    #Windows Powershell only
+    If ($PSVersionTable.PSVersion.Major -eq 5 ) {
+        Get-WmiObject win32_operatingsystem | Select-Object @{EXPRESSION = { $_.ConverttoDateTime($_.lastbootuptime) } } | Format-Table -HideTableHeaders
+    }
+    Else {
+        net statistics workstation | Select-String "since" | foreach-object { $_.ToString().Replace('Statistics since ', '') }
+    }
+}
+
+function reload-profile { & $profile }
+
+function find-file($name) {
+    Get-ChildItem -recurse -filter "*${name}*" -ErrorAction SilentlyContinue | ForEach-Object {
+        $place_path = $_.directory
+        Write-Output "${place_path}\${_}"
+    }
+}
+
+function Keep-Awake {
+    $TimerSeconds = 60 * 3
+    $MyShell = New-Object -ComObject Wscript.Shell
+    while (1) {
+        $MyShell.SendKeys("+{F15}")
+        Write-Output("Keeping awake... $(Get-Date)")
+        Start-Sleep -Seconds $TimerSeconds
+    }
+}
+
+# oh-my-posh --init --shell pwsh --config "~/$env:posh_themes_path/powerlevel10k_rainbow.omp.json" | Invoke-Expression
+oh-my-posh --init --shell pwsh --config "~/.config/oh_my_posh/powerlevel10k_rainbow.omp.json" | Invoke-Expression
