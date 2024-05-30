205a78a857cf224919ee5537717c6c744064c425
Muhammad Hafiz bin Muhamad <mhafiz.muhamad@gmail.com>
2024-05-30 10:20:05 +0800

    Update neovim dev 1. Added treesitter plugins. 2. Updated lazy.

--- neovim/nvim_dev/lua/setup/lazy.lua
+++ neovim/nvim_dev/lua/setup/lazy.lua
@@ -22,6 +22,7 @@ require("lazy").setup(
   {
     {import = "setup.plugins.themes"},
     {import = "setup.plugins.telescope"},
+    {import = "setup.plugins.treesitter"},
     {import = "setup.plugins.git"},
     {import = "setup.plugins.which_key"},
   },
