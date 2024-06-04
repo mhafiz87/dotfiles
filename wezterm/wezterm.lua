f3f789a8e1558eef24be116999a86418194b6875
Muhammad Hafiz bin Muhamad <mhafiz.muhamad@gmail.com>
2024-06-04 08:25:27 +0800

    Update wezterm dotfiles.

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -8,21 +8,25 @@ local wezterm = require("wezterm")
 local mux = wezterm.mux
 local act = wezterm.action
 
+local function basename(s)
+  return string.gsub(s, '(.*[/\\])(.*)', '%2')
+end
+
 -- This will hold the configuration.
 local config = wezterm.config_builder()
 if wezterm.config_builder then
-	config = wezterm.config_builder()
+  config = wezterm.config_builder()
 end
 
 -- This is where you actually apply your config choices
 
 if not platform.is_win then
-	config.default_cwd = "~"
+  config.default_cwd = "~"
 end
 
 if platform.is_win then
-	config.default_cwd = os.getenv("userprofile")
-	config.default_prog = { "pwsh", "-l" }
+  config.default_cwd = os.getenv("userprofile")
+  config.default_prog = { "pwsh", "-l" }
 end
 
 config.automatically_reload_config = true
@@ -31,7 +35,7 @@ config.automatically_reload_config = true
 -- Override scrollbar thumb color
 local scheme = wezterm.get_builtin_color_schemes()["Vs Code Dark+ (Gogh)"]
 scheme.scrollbar_thumb = "#ffffff"
-config.color_schemes = {["Vs Code Dark+ (Gogh)"] = scheme}
+config.color_schemes = { ["Vs Code Dark+ (Gogh)"] = scheme }
 config.color_scheme = "Vs Code Dark+ (Gogh)"
 
 -- UI
@@ -42,10 +46,10 @@ config.hide_mouse_cursor_when_typing = true
 config.hide_tab_bar_if_only_one_tab = true
 config.tab_max_width = 25
 config.window_padding = {
-	left = 5,
-	right = 15,
-	top = 10,
-	bottom = 10,
+  left = 5,
+  right = 15,
+  top = 10,
+  bottom = 10,
 }
 config.inactive_pane_hsb = { saturation = 0.9, brightness = 0.50 }
 config.min_scroll_bar_height = "2cell"
@@ -58,7 +62,7 @@ config.default_cursor_style = "BlinkingBlock"
 
 -- Font Settings
 config.font = wezterm.font_with_fallback({
-	{ family = "JetBrainsMono Nerd Font" },
+  { family = "JetBrainsMono Nerd Font" },
 })
 config.font_size = 10
 
@@ -69,31 +73,45 @@ config.key_tables = key.key_tables
 
 -- Mouse Bindings
 config.mouse_bindings = {
-	-- Hyperlink
-	-- Ctrl-click will open the link under the mouse cursor
-	{
-		event = { Up = { streak = 1, button = "Left" } },
-		mods = "CTRL",
-		action = act.OpenLinkAtMouseCursor,
-	},
+  -- Hyperlink
+  -- Ctrl-click will open the link under the mouse cursor
+  {
+    event = { Up = { streak = 1, button = "Left" } },
+    mods = "CTRL",
+    action = act.OpenLinkAtMouseCursor,
+  },
 }
 
 wezterm.on("gui-startup", function(cmd)
-	local tab, pane, window = mux.spawn_window(cmd or {})
-	window:gui_window():maximize()
+  local tab, pane, window = mux.spawn_window(cmd or {})
+  window:gui_window():maximize()
 end)
 
-function basename(s)
-  return string.gsub(s, '(.*[/\\])(.*)', '%2')
-end
+-- Tab bar
+-- https://github.com/theopn/dotfiles/blob/25b85936ef3e7195a0f029525f854fdb915b9f90/wezterm/wezterm.lua
+config.use_fancy_tab_bar = false
+config.status_update_interval = 1000
+wezterm.on("update-right-status", function(window, pane)
+  -- Workspace name
+  local stat = window:active_workspace()
+  -- It's a little silly to have workspace name all the time
+  -- Utilize this to display LDR or current key table name
+  if window:active_key_table() then stat = window:active_key_table() end
+  if window:leader_is_active() then stat = "LDR" end
+  -- Current process
+  local process = basename(pane:get_foreground_process_name())
+  -- Time
+  local time = wezterm.strftime("%H:%M")
 
-wezterm.on('update-right-status', function(window, pane)
-  local process_info = pane:get_foreground_process_info()
-  if string.find(process_info.executable, "nvim") then
-    window:set_right_status("nvim")
-  else
-    window:set_right_status(basename(process_info.name))
-  end
+  window:set_right_status(wezterm.format({
+    { Text = wezterm.nerdfonts.oct_table .. "  " .. stat },
+    { Text = " | " },
+    { Foreground = { Color = "FFB86C" } },
+    { Text = wezterm.nerdfonts.fa_code .. "  " .. process },
+    "ResetAttributes",
+    { Text = " | " },
+    { Text = wezterm.nerdfonts.md_clock .. "  " .. time },
+    { Text = " |" },
+  }))
 end)
-
 return config
