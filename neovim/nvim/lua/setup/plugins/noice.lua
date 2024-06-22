e1aceb71df0092a5111b66e9df98ca470d3ff8d2
Muhammad Hafiz bin Muhamad <mhafiz.muhamad@gmail.com>
2024-06-22 17:21:32 +0800

    Updated noice plugin to show statusline.

--- neovim/nvim/lua/setup/plugins/noice.lua
+++ neovim/nvim/lua/setup/plugins/noice.lua
@@ -15,6 +15,42 @@ function M.init(args)
     },
     config = function()
       require("noice").setup({
+        -- routes = {
+        --   {
+        --     view = "notify",
+        --     filter = { event = "msg_showmode" },
+        --   },
+        -- },
+        views = {
+          cmdline_popup = {
+            position = {
+              row = 5,
+              col = "50%",
+            },
+            size = {
+              width = 60,
+              height = "auto",
+            },
+          },
+          popupmenu = {
+            relative = "editor",
+            position = {
+              row = 8,
+              col = "50%",
+            },
+            size = {
+              width = 60,
+              height = 10,
+            },
+            border = {
+              style = "rounded",
+              padding = { 0, 1 },
+            },
+            win_options = {
+              winhighlight = { Normal = "Normal", FloatBorder = "DiagnosticInfo" },
+            },
+          },
+        },
         lsp = {
           -- override markdown rendering so that **cmp** and other plugins use **Treesitter**
           override = {
@@ -23,13 +59,19 @@ function M.init(args)
             ["cmp.entry.get_documentation"] = true, -- requires hrsh7th/nvim-cmp
           },
         },
-        -- you can enable a preset for easier configuration
-        presets = {
-          bottom_search = true,         -- use a classic bottom cmdline for search
-          command_palette = true,       -- position the cmdline and popupmenu together
-          long_message_to_split = true, -- long messages will be sent to a split
-          inc_rename = false,           -- enables an input dialog for inc-rename.nvim
-          lsp_doc_border = false,       -- add a border to hover docs and signature help
+      })
+      require("lualine").setup({
+        sections = {
+          lualine_x = {
+            {
+              require("noice").api.statusline.mode.get,
+              cond = require("noice").api.statusline.mode.has,
+              color = { fg = "#ff9e64" },
+            },
+            { "encoding" },
+            { "fileformat" },
+            { "filetype" },
+          },
         },
       })
       require("telescope").load_extension("noice")
