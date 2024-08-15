80f90d6fa903aeb49555b6d37455e06140cfe593
Muhammad Hafiz bin Muhamad <mhafiz.muhamad@gmail.com>
2024-08-15 09:21:30 +0800

    Updated nvim_dev config.
    1. Auto close nvim tree after opening a file.

--- neovim/nvim_dev/lua/setup/plugins/ui/nvim-tree.lua
+++ neovim/nvim_dev/lua/setup/plugins/ui/nvim-tree.lua
@@ -6,7 +6,13 @@ function M.init (args)
     enabled = args.enable,
     "nvim-tree/nvim-tree.lua",
     config = function ()
-      require("nvim-tree").setup({})
+      require("nvim-tree").setup({
+        actions = {
+          open_file = {
+            quit_on_open = true
+          }
+        }
+      })
       vim.keymap.set("n", "<C-k><C-b>", "<CMD>NvimTreeToggle<CR>", { desc = "Toggle Nvim Tree" })
     end
   }
