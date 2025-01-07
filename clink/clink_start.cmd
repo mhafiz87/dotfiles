48e235ea022a9afd0796e8304c294553b7b4d544
Muhammad Hafiz bin Muhamad <mhafiz.muhamad@gmail.com>
2025-01-07 13:07:49 +0800

    clink: updated clink_start.cmd

--- clink/clink_start.cmd
+++ clink/clink_start.cmd
@@ -1,2 +1,2 @@
 set FZF_CTRL_T_OPTS=--preview-window "right:40%%,border-left" --bind "ctrl-/:change-preview-window(right:70%%|hidden|)" --preview "bat --force-colorization --style=numbers,changes --line-range=:500 -- {}"
-  
+cls
