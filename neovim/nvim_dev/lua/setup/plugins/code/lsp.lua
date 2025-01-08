b650711fc441fdb162cf0acda901388ee35f134e
Muhammad Hafiz bin Muhamad <mhafiz.muhamad@gmail.com>
2025-01-08 22:14:48 +0800

    feat(nvim_dev): lsp config based on os

--- neovim/nvim_dev/lua/setup/plugins/code/lsp.lua
+++ neovim/nvim_dev/lua/setup/plugins/code/lsp.lua
@@ -34,37 +34,60 @@ function M.init(args)
       local tools = require("tools")
       local lspconfig = require("lspconfig")
       local capabilities = require('blink.cmp').get_lsp_capabilities()
-      lspconfig.lua_ls.setup({})
+      if global.is_windows then
+        lspconfig.lua_ls.setup({})
 
-      -- https://github.com/LazyVim/LazyVim/discussions/3350#discussioncomment-9584985
-      lspconfig.basedpyright.setup({
-        -- on_attach = on_attach,
-        capabilities = capabilities,
-        settings = {
-          basedpyright = {
-            analysis = {
-              typeCheckingMode = "standard",
-              reportOptionalMemberAccess = "none",
+        -- https://github.com/LazyVim/LazyVim/discussions/3350#discussioncomment-9584985
+        lspconfig.basedpyright.setup({
+          -- on_attach = on_attach,
+          capabilities = capabilities,
+          settings = {
+            basedpyright = {
+              analysis = {
+                typeCheckingMode = "standard",
+                reportOptionalMemberAccess = "none",
+              },
             },
           },
-        },
-      })
+        })
 
-      lspconfig.bashls.setup({
-        -- on_attach = on_attach,
-        capabilities = capabilities,
-        filetypes = { "sh", "bash", "zsh" },
-      })
+        lspconfig.bashls.setup({
+          -- on_attach = on_attach,
+          capabilities = capabilities,
+          filetypes = { "sh", "bash", "zsh" },
+        })
 
-      lspconfig.ruff.setup({
-        -- on_attach = on_attach,
-        capabilities = capabilities,
-      })
+        lspconfig.ruff.setup({
+          -- on_attach = on_attach,
+          capabilities = capabilities,
+        })
 
-      lspconfig.marksman.setup({
-        -- on_attach = on_attach,
-        capabilities = capabilities,
-      })
+        lspconfig.marksman.setup({
+          -- on_attach = on_attach,
+          capabilities = capabilities,
+        })
+      else
+        require("mason").setup()
+        require("mason-lspconfig").setup({
+          automatic_installation = true,
+          ensure_installed = {
+            "lua_ls",
+            "basedpyright",
+            "powershell_es",
+            "marksman",
+            "ruff",
+            "bashls",
+          },
+          handlers = {
+            function(server_name)
+              lspconfig[server_name].setup({
+                on_attach = on_attach,
+                capabilities = capabilities,
+              })
+            end,
+          }
+        })
+      end
     end,
   }
   return data
