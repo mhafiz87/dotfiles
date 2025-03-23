79cb97633489ebfae89647187a2f9f8529b83b5e
Muhammad Hafiz bin Muhamad <mhafiz.muhamad@gmail.com>
2025-03-23 17:53:54 +0800

    chore(nvim-dev): update

--- neovim/nvim-dev/after/ftplugin/python.lua
+++ neovim/nvim-dev/after/ftplugin/python.lua
@@ -1,4 +1,4 @@
-local tools = require("tools")
+local utils = require("utils")
 
 vim.opt.expandtab = true
 vim.opt.autoindent = true
@@ -8,6 +8,6 @@ vim.opt.tabstop = 4
 vim.opt.softtabstop = 4
 vim.opt.colorcolumn = "80,88,120"
 
-vim.keymap.set("n", "<leader>td", tools.toggle_all_python_docstring, { noremap = true, desc = "[t]toggle [d]ocstring", silent = true })
+vim.keymap.set("n", "<leader>td", utils.toggle_all_python_docstring, { noremap = true, desc = "[t]toggle [d]ocstring", silent = true })
 vim.keymap.set("n", "<leader>rf", "<cmd>cexpr system('refurb --quiet ' . shellescape(expand('%'))) | copen<cr>", { noremap = true, desc = "[r]e[f]urb", silent = true })
-vim.keymap.set("v", "<leader>mb", tools.format_selected_lines_using_black, { noremap = true, desc = "For[m]at selected lines using [b]lack", silent = true })
+vim.keymap.set("v", "<leader>mb", utils.format_selected_lines_using_black, { noremap = true, desc = "For[m]at selected lines using [b]lack", silent = true })
