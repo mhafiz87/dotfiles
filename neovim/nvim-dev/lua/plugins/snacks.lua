d570c54eca5a1486fa07bdad226a9ea95b339f7e
Muhammad Hafiz bin Muhamad <mhafiz.muhamad@gmail.com>
2025-06-05 09:18:32 +0800

    chore(nvim-dev): update plugins

--- neovim/nvim-dev/lua/plugins/snacks.lua
+++ neovim/nvim-dev/lua/plugins/snacks.lua
@@ -40,7 +40,20 @@ return {
     quickfile = { enabled = true },
     scope = { enabled = true },
     scroll = { enabled = false },
-    statuscolumn = { enabled = true },
+    statuscolumn = {
+      enabled = true,
+      left = { "mark", "sign" }, -- priority of signs on the left (high to low)
+      right = { "fold", "git" }, -- priority of signs on the right (high to low)
+      folds = {
+        open = true, -- show open fold icons
+        git_hl = false, -- use Git Signs hl for fold icons
+      },
+      git = {
+        -- patterns to match Git signs
+        patterns = { "GitSign", "MiniDiffSign" },
+      },
+      refresh = 50, -- refresh at most every 50ms
+    },
     words = { enabled = true },
     zen = {
       toggles = {
