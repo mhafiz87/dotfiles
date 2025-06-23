4cb4fe50d69739deab734ce3ad394f93764ab079
Muhammad Hafiz bin Muhamad <mhafiz.muhamad@gmail.com>
2025-06-23 09:28:46 +0800

    chore(nvim): update themery

--- nvim/lua/plugins/themery.lua
+++ nvim/lua/plugins/themery.lua
@@ -1,24 +1,71 @@
 return {
-  'zaldih/themery.nvim',
-  lazy = false,
-  priority = 999,
-  dependencies = {
-    {
-      'folke/tokyonight.nvim',
-      priority = 1000,
-      opts = {},
-    },
-    {
-      'loctvl842/monokai-pro.nvim',
-      priority = 1001,
-      opts = {},
-    }
+  {
+    'folke/tokyonight.nvim',
+    priority = 1000,
+    opts = {},
   },
-  opts = {
-    themes = {
-      'monokai-pro',
-      'tokyonight-night',
-    },
-    livePreview = true,
+  {
+    'loctvl842/monokai-pro.nvim',
+    priority = 1001,
+    opts = {},
+  },
+  {
+    "rose-pine/neovim",
+    name = "rose-pine",
+    priority = 1002,
+    config = function()
+      -- vim.cmd("colorscheme rose-pine")
+    end
+  },
+  {
+    'zaldih/themery.nvim',
+    lazy = false,
+    priority = 2000,
+    config = function ()
+      require("themery").setup({
+        globalBefore = [[]],
+        globalAfter = [[]],
+        themes = {
+          {
+            name = "tokyonight night",
+            colorscheme = "tokyonight-night", 
+          },
+          {
+            name = "tokyonight storm",
+            colorscheme = "tokyonight-storm", 
+          },
+          {
+            name = "tokyonight day",
+            colorscheme = "tokyonight-day", 
+          },
+          {
+            name = "tokyonight moon",
+            colorscheme = "tokyonight-moon", 
+          },
+          {
+            name = "monokai pro",
+            colorscheme = "monokai-pro",
+
+          },
+          {
+            name = "monokai pro classic",
+            colorscheme = "monokai-pro-classic",
+          },
+          {
+            name = "rose pine",
+            colorscheme = "rose-pine-main",
+          },
+          {
+            name = "rose pine moon",
+            colorscheme = "rose-pine-moon",
+          },
+          {
+            name = "rose pine dawn",
+            colorscheme = "rose-pine-dawn",
+          },
+        },
+        livePreview = true,
+      })
+    end
   }
 }
