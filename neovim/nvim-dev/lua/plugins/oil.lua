b5a3e3e08eb6d884d0db73e6332ae9bc319f8f09
Muhammad Hafiz bin Muhamad <mhafiz.muhamad@gmail.com>
2025-08-16 06:23:51 +0800

    chore(nvim-dev): update oil plugin

--- neovim/nvim-dev/lua/plugins/oil.lua
+++ neovim/nvim-dev/lua/plugins/oil.lua
@@ -2,9 +2,14 @@ return {
   'stevearc/oil.nvim',
   ---@module 'oil'
   ---@type oil.SetupOpts
-  opts = {},
+  opts = {
+    default_file_explorer = false,
+  },
   -- Optional dependencies
   dependencies = { "nvim-tree/nvim-web-devicons" }, -- use if you prefer nvim-web-devicons
   -- Lazy loading is not recommended because it is very tricky to make it work correctly in all situations.
   lazy = false,
+  config = function (_, opts)
+    require("oil").setup(opts)
+  end
 }
