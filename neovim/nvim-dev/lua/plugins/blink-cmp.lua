56ee67314a967f936cc542838da5c902df55b040
Muhammad Hafiz bin Muhamad <mhafiz.muhamad@gmail.com>
2025-08-17 01:40:24 +0800

    chore(nvim-dev): update config and plugin

--- neovim/nvim-dev/lua/plugins/blink-cmp.lua
+++ neovim/nvim-dev/lua/plugins/blink-cmp.lua
@@ -8,8 +8,10 @@ return {
   enabled = true,
   'saghen/blink.cmp',
   -- optional: provides snippets for the snippet source
-  dependencies = { 'rafamadriz/friendly-snippets' },
-
+  dependencies = {
+    'rafamadriz/friendly-snippets',
+    "xzbdmw/colorful-menu.nvim",
+  },
   -- use a release tag to download pre-built binaries
   version = '1.*',
   ---@module 'blink.cmp'
@@ -62,9 +64,27 @@ return {
       menu = {
         auto_show = true,
         draw = {
+          -- without 'colorful-menu'
+          -- columns = {
+          --   { "label", "label_description", gap = 1 },
+          --   { "kind_icon", "kind", "source_name", gap = 1 }
+          -- },
+          -- with 'colorful-menu'
+          -- We don't need label_description now because label and label_description are already
+          -- combined together in label by colorful-menu.nvim.
           columns = {
-            { "label", "label_description", gap = 1 },
-            { "kind_icon", "kind", "source_name", gap = 1 }
+            { "kind_icon" },
+            { "label", "source_name" ,gap = 1 },
+          },
+          components = {
+            label = {
+              text = function(ctx)
+                return require("colorful-menu").blink_components_text(ctx)
+              end,
+              highlight = function(ctx)
+                return require("colorful-menu").blink_components_highlight(ctx)
+              end,
+            },
           },
         },
       },
@@ -104,8 +124,17 @@ return {
         ['<C-y>'] = { 'select_and_accept' },
         ['<C-e>'] = { 'cancel' },
         -- #TODO: WIP
-        ['<Enter>'] = { 'select_accept_and_enter' },
-      }
+        -- ['<Enter>'] = { 'select_accept_and_enter' },:
+        ['<Enter>'] = {
+          function(cmp)
+            if cmp.is_menu_visible() then
+              return cmp.select_and_accept()
+            end
+          end,
+          "fallback",
+        },
+      },
+      sources = { "buffer", "cmdline" },
     },
 
     -- Default list of enabled providers defined so that you can extend it
