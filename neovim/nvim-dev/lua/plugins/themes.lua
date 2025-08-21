d5b9e6ee763e9cbc40db0e031b25e942259f8c2e
Muhammad Hafiz bin Muhamad <mhafiz.muhamad@gmail.com>
2025-08-21 10:42:35 +0800

    chore(nvim-dev): update config and plugins

--- neovim/nvim-dev/lua/plugins/themes.lua
+++ neovim/nvim-dev/lua/plugins/themes.lua
@@ -4,6 +4,11 @@ return {
     name = "rose-pine",
     config = function()
         -- vim.cmd.colorscheme("rose-pine")
+        require("rose-pine").setup({
+          highlight_groups = {
+            Cursor = { fg = "#fcf0ea", bg = "#fcf0ea" },
+          },
+        })
         vim.cmd [[colorscheme rose-pine]]
     end
 }
