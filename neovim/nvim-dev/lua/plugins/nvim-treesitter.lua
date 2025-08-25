d45c13b6225fa90147e224fb2739e8210cfd963e
Muhammad Hafiz bin Muhamad <mhafiz.muhamad@gmail.com>
2025-08-25 11:10:25 +0800

    chore(nvim-dev): update config and plugins

--- neovim/nvim-dev/lua/plugins/nvim-treesitter.lua
+++ neovim/nvim-dev/lua/plugins/nvim-treesitter.lua
@@ -1,69 +1,122 @@
 return {
-  enabled = true,
-  "nvim-treesitter/nvim-treesitter",
-  event = { "BufReadPre", "BufNewFile" },
-  branch = "master",
-  lazy = false,
-  build = ":TSUpdate",
-  config = function()
-    require'nvim-treesitter.configs'.setup {
-      -- A list of parser names, or "all" (the listed parsers MUST always be installed)
-      ensure_installed = {
-        "dockerfile",
-        "gitcommit",
-        "gitignore",
-        "json",
-        "jsonc",
-        "markdown",
-        "markdown_inline",
-        "lua",
-        "python",
-        "toml",
-        "vim",
-        "vimdoc",
-        "query",
-        "xml",
-        "yaml",
-      },
+  {
+    enabled = true,
+    "nvim-treesitter/nvim-treesitter",
+    event = { "BufReadPre", "BufNewFile" },
+    branch = "master",
+    lazy = false,
+    build = ":TSUpdate",
+    config = function()
+      require'nvim-treesitter.configs'.setup {
+        -- A list of parser names, or "all" (the listed parsers MUST always be installed)
+        ensure_installed = {
+          "dockerfile",
+          "gitcommit",
+          "gitignore",
+          "json",
+          "jsonc",
+          "markdown",
+          "markdown_inline",
+          "lua",
+          "python",
+          "toml",
+          "vim",
+          "vimdoc",
+          "query",
+          "xml",
+          "yaml",
+        },
 
-      -- Install parsers synchronously (only applied to `ensure_installed`)
-      sync_install = false,
+        -- Install parsers synchronously (only applied to `ensure_installed`)
+        sync_install = false,
 
-      -- Automatically install missing parsers when entering buffer
-      -- Recommendation: set to false if you don't have `tree-sitter` CLI installed locally
-      auto_install = false,
+        -- Automatically install missing parsers when entering buffer
+        -- Recommendation: set to false if you don't have `tree-sitter` CLI installed locally
+        auto_install = false,
 
-      -- List of parsers to ignore installing (or "all")
-      -- ignore_install = { "javascript" },
+        -- List of parsers to ignore installing (or "all")
+        -- ignore_install = { "javascript" },
 
-      ---- If you need to change the installation directory of the parsers (see -> Advanced Setup)
-      -- parser_install_dir = "/some/path/to/store/parsers", -- Remember to run vim.opt.runtimepath:append("/some/path/to/store/parsers")!
+        ---- If you need to change the installation directory of the parsers (see -> Advanced Setup)
+        -- parser_install_dir = "/some/path/to/store/parsers", -- Remember to run vim.opt.runtimepath:append("/some/path/to/store/parsers")!
 
-      highlight = {
-        enable = true,
+        highlight = {
+          enable = true,
 
-        -- NOTE: these are the names of the parsers and not the filetype. (for example if you want to
-        -- disable highlighting for the `tex` filetype, you need to include `latex` in this list as this is
-        -- the name of the parser)
-        -- list of language that will be disabled
+          -- NOTE: these are the names of the parsers and not the filetype. (for example if you want to
+          -- disable highlighting for the `tex` filetype, you need to include `latex` in this list as this is
+          -- the name of the parser)
+          -- list of language that will be disabled
 
-        -- disable = { "c", "rust" },
+          -- disable = { "c", "rust" },
 
-        -- Or use a function for more flexibility, e.g. to disable slow treesitter highlight for large files
-        disable = function(lang, buf)
+          -- Or use a function for more flexibility, e.g. to disable slow treesitter highlight for large files
+          disable = function(lang, buf)
             local max_filesize = 100 * 1024 -- 100 KB
             local ok, stats = pcall(vim.loop.fs_stat, vim.api.nvim_buf_get_name(buf))
             if ok and stats and stats.size > max_filesize then
-                return true
+              return true
             end
-        end,
+          end,
 
-        -- Setting this to true will run `:h syntax` and tree-sitter at the same time.
-        -- Set this to `true` if you depend on 'syntax' being enabled (like for indentation).
-        -- Using this option may slow down your editor, and you may see some duplicate highlights.
-        -- Instead of true it can also be a list of languages
-        additional_vim_regex_highlighting = false,
-      },
+          -- Setting this to true will run `:h syntax` and tree-sitter at the same time.
+          -- Set this to `true` if you depend on 'syntax' being enabled (like for indentation).
+          -- Using this option may slow down your editor, and you may see some duplicate highlights.
+          -- Instead of true it can also be a list of languages
+          additional_vim_regex_highlighting = false,
+        },
+      }
+    end
+  },
+  {
+    "nvim-treesitter/nvim-treesitter-context",
+    priority = 995,
+    event = { "BufReadPost", "BufNewFile" },
+    dependencies = {
+      "nvim-treesitter/nvim-treesitter",
+    },
+    opts = {
+      max_lines = 5,
     }
-  end
+  },
+  {
+    "nvim-treesitter/nvim-treesitter-textobjects",
+    priority = 994,
+    event = { "BufReadPost", "BufNewFile" },
+    dependencies = {
+      "nvim-treesitter/nvim-treesitter",
+    },
+    keys = {
+      { "]m", function ()
+        require("nvim-treesitter.textobjects.move").goto_next_start("@function.outer")
+        vim.cmd(":sleep 100m")
+        vim.cmd("norm! zz")
+      end , desc = "Next function", noremap = true, silent = true },
+      { "[m", function ()
+        require("nvim-treesitter.textobjects.move").goto_previous_start("@function.outer")
+        vim.cmd(":sleep 100m")
+        vim.cmd("norm! zz")
+      end , desc = "Next function", noremap = true, silent = true },
+      { "]-", function ()
+        require("nvim-treesitter.textobjects.move").goto_next_start("@function.outer")
+        vim.cmd(":sleep 100m")
+        vim.cmd("norm! zz")
+      end , desc = "Next function", noremap = true, silent = true },
+      { "[-", function ()
+        require("nvim-treesitter.textobjects.move").goto_previous_start("@function.outer")
+        vim.cmd(":sleep 100m")
+        vim.cmd("norm! zz")
+      end , desc = "Next function", noremap = true, silent = true },
+      { "}", function ()
+        require("nvim-treesitter.textobjects.move").goto_next_start("@block.outer")
+        vim.cmd(":sleep 100m")
+        vim.cmd("norm! zz")
+      end , desc = "Next block", noremap = true, silent = true },
+      { "{", function ()
+        require("nvim-treesitter.textobjects.move").goto_previous_start("@block.outer")
+        vim.cmd(":sleep 100m")
+        vim.cmd("norm! zz")
+      end , desc = "Previous block", noremap = true, silent = true },
+    },
+  }
 }
