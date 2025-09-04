1a423054fdcf9891986b81df2bdfa079b7be8540
Muhammad Hafiz bin Muhamad <mhafiz.muhamad@gmail.com>
2025-09-04 17:26:55 +0800

    chore(nvim-dev): update snacks plugins

--- neovim/nvim-dev/lua/plugins/snacks.lua
+++ neovim/nvim-dev/lua/plugins/snacks.lua
@@ -9,14 +9,21 @@ return {
   keys = {
     -- buffers
     { "<leader>bf", function() Snacks.picker.buffers() end, desc = "[b]uffer [f]ind" },
+    { "<leader>bd", function() Snacks.bufdelete(0) end, desc = "[b]uffer current [d]elete" },
+    { "<leader>bo", function() Snacks.bufdelete.other() end, desc = "[b]uffer delete [o]thers" },
+    -- diagnostic
     -- find
     { "<leader>ff", function() Snacks.picker.files() end, desc = "[f]ind [f]iles" },
+    { "<leader>fm", function() Snacks.picker.marks() end, desc = "[f]ind [m]arks" },
     -- git
     { "<leader>ghf", function() Snacks.picker.git_diff() end, desc = "[g]it [h]unk [f]ind" },
+    { "<leader>ghs", function() Snacks.picker.git_status() end, desc = "[g]it [h]unk [s]tatus" },
+    { "<leader>ghb", function() Snacks.picker.git_branches() end, desc = "[g]it [h]unk [b]ranches" },
+    { "<leader>ghd", function() Snacks.picker.git_diff() end, desc = "[g]it [h]unk [d]iff" },
     -- lsp
     { "<leader>grr", function() Snacks.picker.lsp_references() end, desc = "lsp references" },
     { "<leader>gri", function() Snacks.picker.lsp_implementations() end, desc = "lsp implementationj" },
-    -- diagnostic
+    -- marks
   },
   config = function (_, opts)
     local snacks = require("snacks")
