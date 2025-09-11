07c0e8817ff303ecee3fb4c7166ee87152eca6da
Muhammad Hafiz bin Muhamad <mhafiz.muhamad@gmail.com>
2025-09-11 08:59:02 +0800

    chore(nvim-dev): remove nvim-dev

--- neovim/nvim-dev/after/ftplugin/lua.lua
+++
@@ -1,5 +0,0 @@
-local set = vim.opt_local
-
-set.shiftwidth = 2  -- tab size
-set.softtabstop = 2  -- tab size
-set.colorcolumn = "80,120"
--- neovim/nvim-dev/lua/plugins/bullets.lua
+++
@@ -1,7 +0,0 @@
-return {
-  "bullets-vim/bullets.vim",
-  ft = "markdown",
-  config = function ()
-    vim.g.bullets_delete_last_bullet_if_empty = 1
-  end
-}
