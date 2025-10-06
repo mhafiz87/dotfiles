c83f0e84f8327f5c433d8542db92020310a0519d
Muhammad Hafiz bin Muhamad <mhafiz.muhamad@gmail.com>
2025-10-06 10:12:59 +0800

    chore(nvim): add bash treesitter

--- nvim/lua/plugins/nvim-treesitter.lua
+++ nvim/lua/plugins/nvim-treesitter.lua
@@ -10,6 +10,7 @@ return {
       require'nvim-treesitter.configs'.setup {
         -- A list of parser names, or "all" (the listed parsers MUST always be installed)
         ensure_installed = {
+          "bash",
           "dockerfile",
           "gitcommit",
           "gitignore",
