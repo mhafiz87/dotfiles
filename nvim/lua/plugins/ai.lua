4644eb9b8757858a5ce626c143c6b93272872acc
Muhammad Hafiz bin Muhamad <mhafiz.muhamad@gmail.com>
2025-10-28 23:10:37 +0800

    chore(nvim): update plugin

---
+++ nvim/lua/plugins/ai.lua
@@ -0,0 +1,26 @@
+return {
+    "monkoose/neocodeium",
+    event = "VeryLazy",
+    config = function ()
+      local neocodeium = require("neocodeium")
+      neocodeium.setup()
+      vim.keymap.set("i", "<A-f>", function()
+          require("neocodeium").accept()
+      end, {desc="[neocodeium] accept"})
+      vim.keymap.set("i", "<A-w>", function()
+          require("neocodeium").accept_word()
+      end, {desc="[neocodeium] accept word"})
+      vim.keymap.set("i", "<A-a>", function()
+          require("neocodeium").accept_line()
+      end, {desc="[neocodeium] accept line"})
+      vim.keymap.set("i", "<A-e>", function()
+          require("neocodeium").cycle_or_complete()
+      end, {desc="[neocodeium] cycle or complete"})
+      vim.keymap.set("i", "<A-r>", function()
+          require("neocodeium").cycle_or_complete(-1)
+      end, {desc="[neocodeium] cycle or complete"})
+      vim.keymap.set("i", "<A-c>", function()
+          require("neocodeium").clear()
+      end, {desc="[neocodeium] clear"})
+    end
+}
