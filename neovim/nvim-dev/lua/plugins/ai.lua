26e6014058b3b192a9ce708cba62e57fa5cdf3e1
Muhammad Hafiz bin Muhamad <mhafiz.muhamad@gmail.com>
2026-01-20 17:56:43 +0800

    chore: nvim-dev, allow --cmd ai_status

--- neovim/nvim-dev/lua/plugins/ai.lua
+++ neovim/nvim-dev/lua/plugins/ai.lua
@@ -1,8 +1,19 @@
 local prefix = "<Leader>a"
+local enabled = true
+
+-- if global variable is not nil
+-- if vim.g.ai_status ~= nil then
+--   enabled = false
+-- end
+
+if vim.g.ai_status == 1 then
+  enabled = false
+end
 
 return {
   {
     "zbirenbaum/copilot.lua",
+    enabled = enabled,
     cmd = "Copilot",
     opts = {
       suggestion = { enabled = false },
