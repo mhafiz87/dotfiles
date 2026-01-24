d9407204a25f2f3f234d74df8dc7926e873671b1
Muhammad Hafiz bin Muhamad <mhafiz.muhamad@gmail.com>
2026-01-25 01:31:58 +0800

    chore: update nvim-dev with plugins from nvim

---
+++ neovim/nvim-dev/lua/plugins/dropbar.lua
@@ -0,0 +1,23 @@
+local utils = require("utils")
+local build = ""
+if utils.is_windows() then
+  -- build = "cmake -S. -Bbuild -DCMAKE_BUILD_TYPE=Release && cmake --build build --config Release"
+  -- https://github.com/nvim-telescope/telescope-fzf-native.nvim/issues/122#issuecomment-2657633030
+  build = "cmake -S. -Bbuild -DCMAKE_BUILD_TYPE=Release && cmake --build build --config Release && cmake --install build --prefix build"
+else
+  build = "make"
+end
+
+return {
+  enabled = true,
+  'Bekaboo/dropbar.nvim',
+  -- optional, but required for fuzzy finder support
+  dependencies = {
+    'nvim-telescope/telescope-fzf-native.nvim',
+    build = build
+  },
+  config = function()
+    local dropbar_api = require('dropbar.api')
+    vim.keymap.set( "n", "<leader>wb", dropbar_api.pick, { desc = "[w]inbar [b]avigation", noremap = true, silent = true })
+  end
+}
